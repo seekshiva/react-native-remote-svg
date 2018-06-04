47cfb3f84e605b41511e22c3a47c6efefc17dd3e
Alex gnatrow <lesha.gnat@gmail.com>
2018-06-05 00:14:57 +0300

    Add native web module for Android

--- SvgImage.js
+++ SvgImage.js
@@ -1,11 +1,15 @@
 // @flow
 
-import React, { Component } from 'react';
-import { View, WebView } from 'react-native';
+import React, { Component } from "react";
+import { View, WebView, Platform } from "react-native";
+
+ //For better performance in Android platform
+import WebAndroid from "react-native-webview-android";
+const isAndroid = Platform.OS === "android";
 
 const firstHtml =
-  '<html><head><style>html, body { margin:0; padding:0; overflow:hidden; background-color: transparent; } svg { position:fixed; top:0; left:0; height:100%; width:100% }</style></head><body>';
-const lastHtml = '</body></html>';
+  "<html><head><style>html, body { margin:0; padding:0; overflow:hidden; background-color: transparent; } svg { position:fixed; top:0; left:0; height:100%; width:100% }</style></head><body>";
+const lastHtml = "</body></html>";
 
 class SvgImage extends Component {
   state = { fetchingUrl: null, svgContent: null };
@@ -19,17 +23,17 @@ class SvgImage extends Component {
     let uri = props.source && props.source.uri;
     if (uri) {
       if (uri.match(/^data:image\/svg/)) {
-        const index = uri.indexOf('<svg');
+        const index = uri.indexOf("<svg");
         this.setState({ fetchingUrl: uri, svgContent: uri.slice(index) });
       } else {
-        console.log('fetching', uri);
+        console.log("fetching", uri);
         fetch(uri)
           .then(res => res.text())
           .then(text => {
             this.setState({ fetchingUrl: uri, svgContent: text });
           })
           .catch(err => {
-            console.error('got error', err);
+            console.error("got error", err);
           });
       }
     }
@@ -37,18 +41,19 @@ class SvgImage extends Component {
   render() {
     const props = this.props;
     const { svgContent } = this.state;
+    const WView = isAndroid ? WebAndroid : WebView
     if (svgContent) {
       return (
         <View pointerEvents="none" style={[props.style, props.containerStyle]}>
-          <WebView
+          <WView
             scalesPageToFit={true}
             style={[
               {
                 width: 200,
                 height: 100,
-                backgroundColor: 'transparent',
+                backgroundColor: "transparent"
               },
-              props.style,
+              props.style
             ]}
             scrollEnabled={false}
             source={{ html: `${firstHtml}${svgContent}${lastHtml}` }}
