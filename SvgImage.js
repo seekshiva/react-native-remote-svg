8d9250f9bd533ab4e503bee98ae2d7442b0866d1
Manoj Mohan <jesuismanoj@gmail.com>
2019-02-11 17:24:38 +0530

    Remove source html encoding for IOS

--- SvgImage.js
+++ SvgImage.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import { View, WebView } from 'react-native';
+import { View, WebView, Platform } from 'react-native';
 
 const firstHtml =
   '<html><head><style>html, body { margin:0; padding:0; overflow:hidden; background-color: transparent; } svg { position:fixed; top:0; left:0; height:100%; width:100% }</style></head><body>';
@@ -56,8 +56,7 @@ class SvgImage extends Component {
               props.style,
             ]}
             scrollEnabled={false}
-            source={{ html: encodeURIComponent(`${firstHtml}${svgContent}${lastHtml}`) }}
-          />
+            source={{ html: Platform.OS === 'ios' ? `${firstHtml}${svgContent}${lastHtml}` : encodeURIComponent(`${firstHtml}${svgContent}${lastHtml}`) }}          />
         </View>
       );
     } else {
