ed06f3af44aa7b6ca963371752bae0d91dc07e8b
Michael Locher <michael_locher@web.de>
2020-10-12 11:27:02 +0200

    Update SvgImage.js

--- SvgImage.js
+++ SvgImage.js
@@ -18,8 +18,8 @@ const getHTML = (svgContent, style) => `
         position: fixed;
         top: 0;
         left: 0;
-        height: 100%;
-        width: 100%;
+        height: 100vh;
+        width: 100vw;
         overflow: hidden;
       }
     </style>
