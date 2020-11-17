87a945299c9df18e53b79742516b6e6f22ea87a1
Matheus Calixto <matheuscalixtobrasil@gmail.com>
2020-11-17 16:57:02 -0300

    Update

--- SvgImage.js
+++ SvgImage.js
@@ -1,8 +1,8 @@
 // @flow
 
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 
 const getHTML = (svgContent, style) => `
 <html data-key="key-${style.height}-${style.width}">
@@ -32,75 +32,65 @@ const getHTML = (svgContent, style) => `
 </html>
 `;
 
-class SvgImage extends Component {
-  state = { fetchingUrl: null, svgContent: null };
-  componentDidMount() {
-    this.doFetch(this.props);
-  }
-  componentWillReceiveProps(nextProps) {
-    const prevUri = this.props.source && this.props.source.uri;
-    const nextUri = nextProps.source && nextProps.source.uri;
+const SvgImage = (props) => {
+  const [fetchingUrl, setFetchingUrl] = useState(null);
+  const [svgContent, setSvgContent] = useState(null);
 
-    if (nextUri && prevUri !== nextUri) {
-      this.doFetch(nextProps);
-    }
-  }
-  doFetch = async props => {
+  useEffect(() => {
+    doFetch();
+  }, []);
+
+  const doFetch = async () => {
     let uri = props.source && props.source.uri;
     if (uri) {
       props.onLoadStart && props.onLoadStart();
       if (uri.match(/^data:image\/svg/)) {
-        const index = uri.indexOf('<svg');
-        this.setState({ fetchingUrl: uri, svgContent: uri.slice(index) });
+        const index = uri.indexOf("<svg");
+        setFetchingUrl(uri);
+        setSvgContent(uri.slice(index));
       } else {
         try {
           const res = await fetch(uri);
           const text = await res.text();
-          this.setState({ fetchingUrl: uri, svgContent: text });
+          setFetchingUrl(uri);
+          setSvgContent(text);
         } catch (err) {
-          console.error('got error', err);
+          console.error("got error", err);
         }
       }
       props.onLoadEnd && props.onLoadEnd();
     }
   };
-  render() {
-    const props = this.props;
-    const { svgContent } = this.state;
-    if (svgContent) {
-      const flattenedStyle = StyleSheet.flatten(props.style) || {};
-      const html = getHTML(svgContent, flattenedStyle);
+  if (svgContent) {
+    const flattenedStyle = StyleSheet.flatten(props.style) || {};
+    const html = getHTML(svgContent, flattenedStyle);
 
-      return (
-        <View pointerEvents="none" style={[props.style, props.containerStyle]}>
-          <WebView
-            originWhitelist={['*']}
-            scalesPageToFit={true}
-            useWebKit={false}
-            style={[
-              {
-                width: 200,
-                height: 100,
-                backgroundColor: 'transparent',
-              },
-              props.style,
-            ]}
-            scrollEnabled={false}
-            showsHorizontalScrollIndicator={false}
-            showsVerticalScrollIndicator={false}
-            source={{ html }}
-          />
-        </View>
-      );
-    } else {
-      return (
-        <View
-          pointerEvents="none"
-          style={[props.containerStyle, props.style]}
+    return (
+      <View pointerEvents="none" style={[props.style, props.containerStyle]}>
+        <WebView
+          originWhitelist={["*"]}
+          scalesPageToFit={true}
+          useWebKit={false}
+          style={[
+            {
+              width: 200,
+              height: 100,
+              backgroundColor: "transparent",
+            },
+            props.style,
+          ]}
+          scrollEnabled={false}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          source={{ html }}
         />
-      );
-    }
+      </View>
+    );
+  } else {
+    return (
+      <View pointerEvents="none" style={[props.containerStyle, props.style]} />
+    );
   }
-}
+};
 
 export default SvgImage;
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Image } from 'react-native';
-import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
-import SvgImage from './SvgImage';
+import React from "react";
+import { Image } from "react-native";
+import SvgImage from "./SvgImage";
 
-const MyImage = props => {
+const MyImage = (props) => {
+  const resolveAssetSource = Image.resolveAssetSource;
   const source = resolveAssetSource(props.source);
-  if (source && (source.uri && source.uri.match('.svg'))) {
+  if (source && source.uri && source.uri.match(".svg")) {
     const style = props.style || {};
-    if (source.__packager_asset && typeof style !== 'number') {
+    if (source.__packager_asset && typeof style !== "number") {
       if (!style.width) {
         style.width = source.width;
       }
