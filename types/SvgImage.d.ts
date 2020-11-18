a218826151433e313a24a44fa69a5adbda99ce6a
Matheus Calixto <matheuscalixtobrasil@gmail.com>
2020-11-18 05:42:28 -0300

    Test types

---
+++ types/SvgImage.d.ts
@@ -0,0 +1,9 @@
+export function SvgImage({ source: { uri }, style, containerStyle, onLoadStart, onLoadEnd, }: {
+    source: {
+        uri: any;
+    };
+    style: any;
+    containerStyle: any;
+    onLoadStart: any;
+    onLoadEnd: any;
+}): any;
---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+export default MyImage;
+declare function MyImage({ source, style, ...props }: {
+    [x: string]: any;
+    source: any;
+    style?: {};
+}): any;
