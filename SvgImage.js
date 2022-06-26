795cb6440c0717a7369ec23fae844154d4ce6ec2
Shiva Nandan <shiva.nandan@juspay.in>
2022-06-26 08:51:34 +0530

    Upgrade SvgImage to functional component
    This also removes the use of deprecated componentWillReceiveProps method

--- SvgImage.js
+++ SvgImage.js
@@ -1,8 +1,8 @@
 // @flow
 
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 
 const getHTML = (svgContent, style) => `
 <html data-key="key-${style.height}-${style.width}">
@@ -32,75 +32,67 @@ const getHTML = (svgContent, style) => `
 </html>
 `;
 
-class SvgImage extends Component {
-  state = { fetchingUrl: null, svgContent: null };
-  componentDidMount() {
-    this.doFetch(this.props);
-  }
-  componentWillReceiveProps(nextProps) {
-    const prevUri = this.props.source && this.props.source.uri;
-    const nextUri = nextProps.source && nextProps.source.uri;
+function SvgImage({ source, onLoadStart, onLoadEnd, style, containerStyle }) {
+  const [_fetchingUrl, setFetchingUrl] = useState(null);
+  const [svgContent, setSvgContent] = useState(null);
 
-    if (nextUri && prevUri !== nextUri) {
-      this.doFetch(nextProps);
-    }
-  }
-  doFetch = async props => {
-    let uri = props.source && props.source.uri;
-    if (uri) {
-      props.onLoadStart && props.onLoadStart();
-      if (uri.match(/^data:image\/svg/)) {
-        const index = uri.indexOf('<svg');
-        this.setState({ fetchingUrl: uri, svgContent: uri.slice(index) });
-      } else {
-        try {
-          const res = await fetch(uri);
-          const text = await res.text();
-          this.setState({ fetchingUrl: uri, svgContent: text });
-        } catch (err) {
-          console.error('got error', err);
+  const uri = source && source.uri;
+  useEffect(() => {
+    async function doFetch() {
+      if (uri) {
+        onLoadStart && onLoadStart();
+        if (uri.match(/^data:image\/svg/)) {
+          const index = uri.indexOf("<svg");
+          setFetchingUrl(uri);
+          setSvgContent(uri.slice(index));
+        } else {
+          try {
+            const res = await fetch(uri);
+            const text = await res.text();
+            setFetchingUrl(uri);
+            setSvgContent(text);
+          } catch (err) {
+            console.error("got error", err);
+          }
         }
+        onLoadEnd && onLoadEnd();
       }
-      props.onLoadEnd && props.onLoadEnd();
     }
-  };
-  render() {
-    const props = this.props;
-    const { svgContent } = this.state;
-    if (svgContent) {
-      const flattenedStyle = StyleSheet.flatten(props.style) || {};
-      const html = getHTML(svgContent, flattenedStyle);
 
-      return (
-        <View pointerEvents="none" style={[props.style, props.containerStyle]}
-          renderToHardwareTextureAndroid={true}>
-          <WebView
-            originWhitelist={['*']}
-            scalesPageToFit={true}
-            useWebKit={false}
-            style={[
-              {
-                width: 200,
-                height: 100,
-                backgroundColor: 'transparent',
-              },
-              props.style,
-            ]}
-            scrollEnabled={false}
-            showsHorizontalScrollIndicator={false}
-            showsVerticalScrollIndicator={false}
-            source={{ html }}
-          />
-        </View>
-      );
-    } else {
-      return (
-        <View
-          pointerEvents="none"
-          style={[props.containerStyle, props.style]}
+    doFetch();
+  }, [uri]);
+
+  if (svgContent) {
+    const flattenedStyle = StyleSheet.flatten(style) || {};
+    const html = getHTML(svgContent, flattenedStyle);
+
+    return (
+      <View
+        pointerEvents="none"
+        style={[style, containerStyle]}
+        renderToHardwareTextureAndroid={true}
+      >
+        <WebView
+          originWhitelist={["*"]}
+          scalesPageToFit={true}
+          useWebKit={false}
+          style={[
+            {
+              width: 200,
+              height: 100,
+              backgroundColor: "transparent",
+            },
+            style,
+          ]}
+          scrollEnabled={false}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          source={{ html }}
         />
-      );
-    }
+      </View>
+    );
+  } else {
+    return <View pointerEvents="none" style={[containerStyle, style]} />;
   }
 }
 
